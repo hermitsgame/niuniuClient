b6d756808b13551ce19243b254a58ed5981d7104
史琦 <569692747@qq.com>
2017-11-03 17:21:37 +0800

    11/3 基本完成俱乐部功能

--- assets/Script/component/other/pomeloClient.js
+++ assets/Script/component/other/pomeloClient.js
@@ -657,9 +657,9 @@ pomelo.goldQuite = function() {
 //         });
 //     });
 // };
-// confige.host = "39.108.139.132"; //测试外网2
+confige.host = "39.108.139.132"; //测试外网2
 // confige.host = "update.5d8d.com";    //测试外网
-confige.host = "nnapi.5d8d.com";     //运营外网
+// confige.host = "nnapi.5d8d.com";     //运营外网
 // confige.host = "192.168.1.65";          //内网
 pomelo.clientLogin = function(uid,clientLogintoken) {
     console.log("pomelo try to login!!!!!!");
